d0d041810ac3bebbfdb5873be6dd039200850090
helderdoutel <heldergomesdoutel@hotmail.com>
2015-12-02 19:47:46 -0300

    leitura do arquivo tsp

--- caixeiro.c
+++ caixeiro.c
@@ -3,24 +3,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-int distancia(int xi, int yi, int xj, int yj){
+#define MAX 300
+int distancia2d(int xi, int yi, int xj, int yj){
 	int distemp, distancia;
-	distemp = ((xi-xj)^2) + ((yi + yj)^2);
+	distemp = ((xi-xj)*(xi-xj)) + ((yi - yj)*(yi - yj));
 	distancia = sqrt(distemp);
 	return distancia;
 }
 
-int main()
-{
-	int a[] = {1,2,3,4};
-	int x=0, y=0;
+int distancia3d(int xi, int yi, int xj, int yj, int zi, int zj){
+	int distemp, distancia;
+	distemp = ((xi-xj)*(xi-xj)) + ((yi - yj)*(yi - yj)) + ((zi - zj)*(zi - zj));
+	distancia = sqrt(distemp);
+	return distancia;
+}
 
-	for(x=0;x<4;x++){
-		for(y=0;y<4;y++){
-			if(y > x){
-				printf("%d %d\n", a[x], a[y]);
+int main(){
+	int i = 0, dim, j, x, y, z, tmp;
+	char ch[MAX], aux[10];
+
+	FILE *f = fopen("test.tsp", "r");
+
+	for(i=0;strcmp(ch, "EOF") != 0;i++){	
+		if(i == 3){
+			fscanf(f, "%s", aux); // Consome "DIMENSION:"
+			fscanf(f, "%d", &dim); // Pega o número de cidades
+			printf("%s %d\n",aux, dim);
+		}
+		if(i > 3){
+			fgets(ch, MAX, f); // consome \n
+			fgets(ch, MAX, f); // consome tipo arquivo
+			if(ch[22] == '2'){ // Verifica se existe EUC_2D
+				fgets(ch, MAX, f); // Pula linha
+				int vtc[dim][3];
+				for(j=0;j<dim;j++){
+					fscanf(f, "%d %d %d", &tmp, &x, &y);
+					vtc[j][0] = tmp;
+					vtc[j][1] = x;
+					vtc[j][2] = y;
+				}
+				for(j=0;j<dim;j++){
+					printf("id=%d\n", vtc[j][0]);
+					printf("x=%d ", vtc[j][1]);
+					printf("y=%d\n", vtc[j][2]);
+				}
+			}
+
+			if(ch[22] == '3'){ // Verifica se existe EUC_2D
+				fgets(ch, MAX, f); // Pula linha
+				int vtc[dim][4];
+				for(j=0;j<dim;j++){
+					fscanf(f, "%d %d %d %d", &tmp, &x, &y, &z);
+					vtc[j][0] = tmp;
+					vtc[j][1] = x;
+					vtc[j][2] = y;
+					vtc[j][3] = z;
+				}
+				for(j=0;j<dim;j++){
+					printf("id=%d\n", vtc[j][0]);
+					printf("x=%d ", vtc[j][1]);
+					printf("y=%d ", vtc[j][2]);
+					printf("z=%d\n", vtc[j][3]);
+				}
+			}
+
+			if(strcmp(ch, "EOF")){ // Verifica se existe EXPLICIT
+				printf("acabou\n");
+				break;
 			}
 		}
+		if(i < 3){
+			fgets(ch, MAX, f); // Armazena cada linha em ch
+			printf("%s", ch);
+		}
 	}
+
 	return 0;
-}
+}	
