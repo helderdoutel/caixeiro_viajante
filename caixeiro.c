e47ffa31f928cf9251824a745c6f3c16c7f9fa7a
helderdoutel <heldergomesdoutel@hotmail.com>
2015-12-03 15:50:23 -0300

    permutacao

--- caixeiro.c
+++ caixeiro.c
@@ -4,6 +4,46 @@
 #include <string.h>
 
 #define MAX 300
+ 
+/* Function to swap values at two pointers */
+void swap(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+ 
+/* Function to print permutations of string
+   This function takes three parameters:
+   1. String
+   2. Starting index of the string
+   3. Ending index of the string. */
+void permute(char *a, int l, int r)
+{
+   int i;
+   if (l == r)
+     printf("%s\n", a);
+   else
+   {
+       for (i = l; i <= r; i++)
+       {
+          swap((a+l), (a+i));
+          permute(a, l+1, r);
+          swap((a+l), (a+i)); //backtrack
+       }
+   }
+}
+ 
+/* Driver program to test above functions */
+int main()
+{
+    char str[] = "123456789";
+    int n = strlen(str);
+    permute(str, 0, n-1);
+    return 0;
+}
+
 int distancia2d(int xi, int yi, int xj, int yj){
 	int distemp, distancia;
 	distemp = ((xi-xj)*(xi-xj)) + ((yi - yj)*(yi - yj));
@@ -47,9 +87,12 @@ int main(){
 					printf("x=%d ", vtc[j][1]);
 					printf("y=%d\n", vtc[j][2]);
 				}
+				// char str[] = "123456789";
+			 //    int n = strlen(str);
+			 //    permute(str, 0, n-1);
 			}
 
-			if(ch[22] == '3'){ // Verifica se existe EUC_2D
+			if(ch[22] == '3'){ // Verifica se existe EUC_3D
 				fgets(ch, MAX, f); // Pula linha
 				int vtc[dim][4];
 				for(j=0;j<dim;j++){
@@ -65,6 +108,7 @@ int main(){
 					printf("y=%d ", vtc[j][2]);
 					printf("z=%d\n", vtc[j][3]);
 				}
+
 			}
 
 			if(strcmp(ch, "EOF")){ // Verifica se existe EXPLICIT
